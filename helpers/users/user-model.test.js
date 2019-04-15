0173799fde68dae724710d513cd5a3e60f8b4e62
GianBarGian <giacomobenati@hotmail.it>
2019-04-15 21:06:37 +0200

    added a simple test for every user-model function

--- helpers/users/user-model.test.js
+++ helpers/users/user-model.test.js
@@ -22,10 +22,28 @@ describe('user-model tests', () => {
     describe('findBy tests', () => {
         it('find the user by the correct param and return his id', async () => {
             await users.add(mockedUser);
+            
             let foundUser = await users.findBy({ username: mockedUser.username });
             expect(foundUser.email).toEqual(mockedUser.email);
+            
             foundUser = await users.findBy({ email: mockedUser.email })
             expect(foundUser.username).toEqual(mockedUser.username);
+        })
+    })
+
+    describe('add tests', () => {
+        it('add a user to the table and return the created user', async () => {
+            const newUser = await users.add(mockedUser);
+            expect(newUser.username).toEqual(mockedUser.username);
+        })
+    })
+
+    describe('findById tests', () => {
+        it('find a user by id', async () => {
+            const newUser = await users.add(mockedUser);
+
+            const foundUser = await users.findById(newUser.id);
+            expect(foundUser.username).toEqual(newUser.username);
         })
     })
 })
