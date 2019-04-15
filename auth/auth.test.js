b51daa06ca05a4b0b8c5cc0f846f5bb06d4221ce
GianBarGian <giacomobenati@hotmail.it>
2019-04-16 00:29:55 +0200

    wip

--- auth/auth.test.js
+++ auth/auth.test.js
@@ -5,7 +5,7 @@ const server = require('../api/server');
 
 describe('auth tests', () => {
     beforeEach(async () => await db('user').truncate());
-    describe('register endpoint tests', () => {
+    describe('POST /register endpoint tests', () => {
         it('return 201 status if a user is registered correctly', async () => {
             const response = await request(server).post('/api/auth/register').send({ username: 'username', password: 'password' });
             expect(response.status).toEqual(201);
@@ -16,14 +16,55 @@ describe('auth tests', () => {
             expect(response.body.username).toEqual('username');
         })
 
-        it('return 401 status if username or password are not provided', async () => {
-            const response = await request(server).post('/api/auth/register').send({ usernae: 'username', password: 'password' });
+        it('return 401 status if username or password are not provided or blank', async () => {
+            let response = await request(server).post('/api/auth/register').send({ usernae: 'username', password: 'password' });
+            expect(response.status).toEqual(401);
+            response = await request(server).post('/api/auth/register').send({ username: '', password: '' });
             expect(response.status).toEqual(401);
         })
 
-        it('return a message error if username or password are not provided', async () => {
-            const response = await request(server).post('/api/auth/register').send({ usernae: 'username', password: 'password' });
+        it('return a message error if username or password are not provided or blank', async () => {
+            let response = await request(server).post('/api/auth/register').send({ username: '', password: '' });
+            expect(response.body).toEqual({ message: 'You need to send an username and a password' });
+            response = await request(server).post('/api/auth/register').send({ usernme: 'asd', password: 'asd' });
             expect(response.body).toEqual({ message: 'You need to send an username and a password' });
         })
     })
+
+    describe('POST /login enpoint tests', () => {
+        it('return 200 status if a user is logged in correctly', async () => {
+            await request(server).post('/api/auth/register').send({ username: 'username', password: 'password' });
+            const response = await request(server).post('/api/auth/login').send({ username: 'username', password: 'password' });
+            expect(response.status).toEqual(200);
+        })
+
+        it('return a token if a user il logged in correctly', async () => {
+            await request(server).post('/api/auth/register').send({ username: 'username', password: 'password' });
+            const response = await request(server).post('/api/auth/login').send({ username: 'username', password: 'password' });
+            expect(response.body.token).toBeTruthy();
+        })
+
+        it('return 401 status if username or password are not provided or wrong', async () => {
+            let response = await request(server).post('/api/auth/login').send({ username: '', password: '' });
+            expect(response.status).toEqual(401);
+            response = await request(server).post('/api/auth/login').send({ username: 'asd', password: 'asd' });
+            expect(response.status).toEqual(401);
+        })
+
+        it('return a message if username or password are not provided or wrong', async () => {
+            let response = await request(server).post('/api/auth/login').send({ username: '', password: '' });
+            expect(response.body).toEqual({ message: 'Try Again' });
+            response = await request(server).post('/api/auth/login').send({ username: 'asd', password: 'asd' });
+            expect(response.body).toEqual({ message: 'Try Again' });
+        })
+    })
+
+
+    // TODO
+    // describe('GET /logout enpoint tests', () => {
+    //     it('return a message if you are logged out correctly', async () => {
+    //         let response = await request(server).get('/api/auth/logout');
+    //         console.log(response);
+    //     })
+    // })
 })
