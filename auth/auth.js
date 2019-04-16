3d3e2f32a73c642bb5f01c964b82c2ad10bb2bd7
Jacob McFaul <mcjakedude@gmail.com>
2019-04-16 13:48:03 -0700

    cors

--- auth/auth.js
+++ auth/auth.js
@@ -61,7 +61,7 @@ router.post('/login', (req, res) => {
             if (user && bcrypt.compareSync(password, user.password)) {
                 const token = generateToken(user);
                 res.status(200)
-                .json({message: `Welcome ${user.username},${user.id}`,token})
+                .json({message: `Welcome ${user.username}`, token,user_id: user.id })
             } else {
                 res.status(401)
                 .json({ message: 'Try Again'})
