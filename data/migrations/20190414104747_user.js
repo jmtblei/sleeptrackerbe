2656d29849e5ef340a606896089be639191ce74f
GianBarGian <giacomobenati@hotmail.it>
2019-04-16 17:27:38 +0200

    wip

--- data/migrations/20190414104747_user.js
+++ data/migrations/20190414104747_user.js
@@ -1,5 +1,5 @@
 
-exports.up = function(knex, Promise) {
+exports.up = function (knex, Promise) {
     return knex.schema.createTable('user', users => {
         users.increments();
         users.string("fullName", 100);
@@ -9,9 +9,24 @@ exports.up = function(knex, Promise) {
             .notNullable()
             .unique();
         users.string("password", 32).notNullable();
+    }).createTable('sleep', sleep => {
+        sleep.increments();
+        sleep
+            .integer("user_id").notNullable()
+            .unsigned()
+            .references("id")
+            .inTable("user")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE");
+
+        sleep.integer("timeSlept").notNullable();
+        sleep.integer("wakeMood");
+        sleep.integer("sleepMood");
+        sleep.date('date', 10);
     })
 };
 
-exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('user');
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('user')
+        .dropTableIfExists('sleep');;
 };
