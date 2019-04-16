475df77f484678a71283357067539d161780af32
GianBarGian <giacomobenati@hotmail.it>
2019-04-16 16:11:22 +0200

    refactored tests structure to run sequentally + addded tests to auth and refactored register endpoint to pass the tests

--- auth/auth.js
+++ auth/auth.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const validator = require('email-validator');
+
 
 const secret = require('../api/secret.js').jwtSecret;
 const User = require('../helpers/users/user-model.js');
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 12)
     user.password = hash;
-    if (user.username && user.password) {
-        User.add(user)
-            .then(saved => {
-                res.status(201).json(saved)
-            })
-            .catch(error => {
-                res.status(500).json(error)
-            })
-    } else {
+    const email = user.email;
+    if (email && !validator.validate(email)) {
+        res.status(400).json({ message: "Please provide a valid email" });
+    } else if (!user.username || !user.password) {
         res.status(401).json({ message: 'You need to send an username and a password' });
+    } 
+    else if (await User.findBy({ username: user.username })) {
+    res.status(400).json({ message: "That username is taken" });
+     } 
+    else {
+        // const existingUser = User.findBy({ username: user.username });
+        // if (existingUser) {
+        //     res.status(400).json({ message: "That username is taken" });
+        // } else {
+            User.add(user)
+                .then(saved => {
+                    res.status(201).json(saved)
+                })
+                .catch(error => {
+                    res.status(500).json(error)
+                })
+        // }
     }
 });
 
---
+++ auth/restrictTests.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+
+const db = require('../data/dbConfig');
+const server = require('../api/server');
+
+module.exports = restrictTests = () => {
+    describe('restrict tests', () => {
+        beforeEach(async () => await db('user').truncate());
+        afterEach(async () => { await db('user').truncate() });
+
+        it('gives back a message if a token is not provided', async () => {
+            const response = await request(server).get('/api/user');
+            expect(response.body).toEqual({ message: 'No token provided' });
+        })
+
+        it('gives back a message if token is incorrect', async () => {
+            const response = await request(server).get('/api/user').set({ Authorization: "wrong token" });
+            expect(response.body).toEqual({ message: 'Invalid!!! Try Again' });
+        })
+
+        it('gives a 401 status if a token is not provided', async () => {
+            const response = await request(server).get('/api/user');
+            expect(response.status).toEqual(401);
+        })
+
+        it('gives a 401 status if a token incorrect', async () => {
+            const response = await request(server).get('/api/user').set({ Authorization: "wrong token" });
+            expect(response.status).toEqual(401);
+        })
+
+        it('gives a 200 status code if the correct token is provided', async () => {
+            await request(server).post('/api/auth/register').send({ username: "giaco", password: 'bena' })
+            const login = await request(server).post('/api/auth/login').send({ username: "giaco", password: 'bena' })
+            const response = await request(server).get('/api/user').set({ Authorization: `${login.body.token}` });
+            expect(response.status).toEqual(200);
+        })
+    })
+} 
