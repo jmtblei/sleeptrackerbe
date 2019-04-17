9737be7ac15cbe39ce7c91215fccf68b2729681b
Jacob McFaul <mcjakedude@gmail.com>
2019-04-17 12:44:08 -0700

    new branch

--- data/seeds/01_user.js
+++ data/seeds/01_user.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('user').insert([
-        {id: 1, username: 'benson', password: '$2a$12$x09gmtgqr39JTeJcG.mN.ebtVoMcSLlWsaMiCt75iO6Jxy680J72m', fullname: 'Benson Lei', email: 'Blei@gmail.com'},
+        {id: 1, username: 'benson', password: '$2a$12$ye.8dx5oEIYv8oxirDTu..2pi6fvDBtIjuOH5XrVED/4.otCYOqhm', fullname: 'Benson Lei', email: 'Blei@gmail.com'},
         {id: 2, username: 'jacob', password: '$2a$12$.Ci1VlQ8Es53gEeshJoLeOsDp1JYuMKqQtMAbFi9jRalx57CnW8pS', fullname: 'Jacob McFaul', email: 'Jmcfaul@gmail.com'},
         {id: 3, username: 'jake', password: '$2a$12$S3MjkSNHpwXQTGZM.hFDreu/T/kUCn6XS5I7yYJBvko2RKHkgcjLm', fullname: 'jake Esveld', email: 'Jesveld@gmail.com'},
         {id: 4, username: 'giacomo', password: '$2a$12$XIy9ZMEO8B5IoFvcqHKpQOh2rWwur1INkAGFjc2eKp83geGZtPOL.', fullname: 'Giacomo Benati', email: 'Gbenati@gmail.com'},
