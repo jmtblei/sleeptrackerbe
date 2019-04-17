3d6540aa92b71abc3d8abc0792967bcde67c2ad2
Jacob McFaul <mcjakedude@gmail.com>
2019-04-17 10:58:12 -0700

    testing

--- helpers/sleepData/sleep-model.js
+++ helpers/sleepData/sleep-model.js
@@ -40,25 +40,4 @@ function update(id, changes){
     .where({ id })
     .update(changes)
 }
-//     async function getAvgTimeSlept(id) {
-//     let today = new Date();
-//     let dd = today.getDate();
-//     let mm = today.getMonth() + 1; //January is 0!
-//     let yyyy = today.getFullYear();
-//     let sixDaysBack = today.getDate() - 6;
-//     let today = yyyy + '-' + mm + '-' + dd;
-//     let sixDaysBack = yyyy + '-' + mm + '-' + dd;
-//     if (dd < 10) {
-//     dd = '0' + dd;
-//     } 
-//     if (mm < 10) {
-//     mm = '0' + mm;
-//     } 
-//     // document.getElementById('DATE').value = today;
-//     const avgTimeSlept = await db('sleep')
-//         .avg('timeSlept as avgTimeSlept')
-//         .whereBetween('date', [sixDaysBack, today])
-//         .andWhere('user_id', id)
-//         .first();
-//     return { avgTimeSlept: Math.round(avgTimeSlept.avgTimeSlept) };
-// }
+
--- helpers/users/user-model.js
+++ helpers/users/user-model.js
@@ -6,7 +6,7 @@ module.exports = {
     findBy,
     findById,
     findSleepByUserId,
-    getAvgTimeSlept
+    // getAvgTimeSlept
 };
 
 function find() {
@@ -35,26 +35,26 @@ function findSleepByUserId(id){
     .where('user.id', id)
 }
 
-async function getAvgTimeSlept(id) {
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1; //January is 0!
-    let yyyy = today.getFullYear();
-    let sixDaysBack = today.getDate() - 6;
-    let today = yyyy + '-' + mm + '-' + dd;
-    let sixDaysBack = yyyy + '-' + mm + '-' + dd;
-    if (dd < 10) {
-    dd = '0' + dd;
-    } 
-    if (mm < 10) {
-    mm = '0' + mm;
-    } 
-    console.log(today)
-    // document.getElementById('DATE').value = today;
-    const avgTimeSlept = await db('sleep')
-        .avg('timeSlept as avgTimeSlept')
-        .whereBetween('date', [sixDaysBack.toString(), today.toString()])
-        .andWhere('user_id', id)
-        .first();
-    return { avgTimeSlept: Math.round(avgTimeSlept.avgTimeSlept) };
-}
+// async function getAvgTimeSlept(id) {
+//     let today = new Date();
+//     let dd = today.getDate();
+//     let mm = today.getMonth() + 1; //January is 0!
+//     let yyyy = today.getFullYear();
+//     let sixDaysBack = today.getDate() - 6;
+//     let today = yyyy + '-' + mm + '-' + dd;
+//     let sixDaysBack = yyyy + '-' + mm + '-' + dd;
+//     if (dd < 10) {
+//     dd = '0' + dd;
+//     } 
+//     if (mm < 10) {
+//     mm = '0' + mm;
+//     } 
+//     console.log(today)
+//     // document.getElementById('DATE').value = today;
+//     const avgTimeSlept = await db('sleep')
+//         .avg('timeSlept as avgTimeSlept')
+//         .whereBetween('date', [sixDaysBack.toString(), today.toString()])
+//         .andWhere('user_id', id)
+//         .first();
+//     return { avgTimeSlept: Math.round(avgTimeSlept.avgTimeSlept) };
+// }
