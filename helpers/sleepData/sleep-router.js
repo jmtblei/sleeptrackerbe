c32d09cc73c7e37dc4c85e4e9ec349cc4b859a72
Jacob McFaul <mcjakedude@gmail.com>
2019-04-17 14:59:58 -0700

    get sleep by id

--- helpers/sleepData/sleep-router.js
+++ helpers/sleepData/sleep-router.js
@@ -10,6 +10,16 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.get('/:id', async(req, res) => {
+    const { id } = req.params
+    try {
+        let sleep = await Sleep.findById(id)
+        res.status(200).json(sleep)
+    } catch(error){
+        res.status(500).json('Thats not a sleep id')
+    }
+})
+
 router.get('/:id/:date', async(req, res) => {
     const { id, date } = req.params
     try {
