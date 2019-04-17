d5f09957ce1b9d514e00c97c79022f16fdef0445
Jacob McFaul <mcjakedude@gmail.com>
2019-04-17 14:53:08 -0700

    updated seeds

--- helpers/sleepData/sleep-router.js
+++ helpers/sleepData/sleep-router.js
@@ -10,9 +10,8 @@ router.get('/', async(req, res) => {
     }
 })
 
-router.get('/:id', async(req, res) => {
-    const { id } = req.params
-    const { date } = req.body
+router.get('/:id/:date', async(req, res) => {
+    const { id, date } = req.params
     try {
         let sleep = await Sleep.findByDate(date, id)
         res.status(200).json(sleep)
