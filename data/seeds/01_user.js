c42cc461d9c5800159a2a5b75c481d56e7372631
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-07-24 17:57:58 -0700

    rs

--- data/seeds/01_user.js
+++ data/seeds/01_user.js
@@ -5,12 +5,12 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('user').insert([
-        {id: 1, username: 'benson', password: '$2a$12$s81x6KdZPhEp6TZ3zLrUxO7Ztzo0SX0HvwUpmINWFGJeCTJZYeAQa', fullname: 'Benson Lei', email: 'Blei@gmail.com'},
-        {id: 2, username: 'jacob', password: '$2a$12$sx6HoqBPo6XFRQi2h8F/.OIbhkU4.Y8iYpChdymkh7lx99Fi/MA4q', fullname: 'Jacob McFaul', email: 'Jmcfaul@gmail.com'},
-        {id: 3, username: 'jake', password: '$2a$12$QIAHyEBmOUksI7yNcvu4MOrVf7RZRKRc96lKhCZJPudinJr6GBsUy', fullname: 'jake Esveld', email: 'Jesveld@gmail.com'},
-        {id: 4, username: 'giacomo', password: '$2a$12$eD1VPDr.pJmzfvraL3A4tO4F5K8HfmPwxfP0zgaCoyWGXMat4mcuC', fullname: 'Giacomo Benati', email: 'Gbenati@gmail.com'},
-        {id: 5, username: 'peter', password: '$2a$12$s81x6KdZPhEp6TZ3zLrUxO7Ztzo0SX0HvwUpmINWFGJeCTJZYeAQa', fullname: 'Peter Pham', email: 'Ppham@gmail.com'},
-        {id: 6, username: 'tyler', password: '$2a$12$BQ08DPSqQ2OBlGXjivv/NebZNLGcz9jxAoqAx7HScX/pye38x.DMK', fullname: 'Tyler Biswell', email: 'Tbiswell@gmail.com'},
+        {id: 1, username: 'benson', password: '$2a$12$s81x6KdZPhEp6TZ3zLrUxO7Ztzo0SX0HvwUpmINWFGJeCTJZYeAQa', fullName: 'Benson Lei', email: 'Blei@gmail.com'},
+        {id: 2, username: 'jacob', password: '$2a$12$sx6HoqBPo6XFRQi2h8F/.OIbhkU4.Y8iYpChdymkh7lx99Fi/MA4q', fullName: 'Jacob McFaul', email: 'Jmcfaul@gmail.com'},
+        {id: 3, username: 'jake', password: '$2a$12$QIAHyEBmOUksI7yNcvu4MOrVf7RZRKRc96lKhCZJPudinJr6GBsUy', fullName: 'jake Esveld', email: 'Jesveld@gmail.com'},
+        {id: 4, username: 'giacomo', password: '$2a$12$eD1VPDr.pJmzfvraL3A4tO4F5K8HfmPwxfP0zgaCoyWGXMat4mcuC', fullName: 'Giacomo Benati', email: 'Gbenati@gmail.com'},
+        {id: 5, username: 'peter', password: '$2a$12$s81x6KdZPhEp6TZ3zLrUxO7Ztzo0SX0HvwUpmINWFGJeCTJZYeAQa', fullName: 'Peter Pham', email: 'Ppham@gmail.com'},
+        {id: 6, username: 'tyler', password: '$2a$12$BQ08DPSqQ2OBlGXjivv/NebZNLGcz9jxAoqAx7HScX/pye38x.DMK', fullName: 'Tyler Biswell', email: 'Tbiswell@gmail.com'},
       ]);
     });
 };
