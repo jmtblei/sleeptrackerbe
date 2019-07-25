6444f37131fd7c49653950de65f5d5bc7db009e8
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-07-24 17:41:33 -0700

    truncate to del

--- data/seeds/02_sleep.js
+++ data/seeds/02_sleep.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('sleep').truncate()
+  return knex('sleep').del()
     .then(function () {
       // Inserts seed entries
       return knex('sleep').insert([
